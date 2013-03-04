7bddd0094e0ea2330eb2a76a09d866603686f223
Tim Gourley <tim.gourley@blinqmedia.com>
2013-03-04 15:34:26 -0600

    Monitoring for railcraft tanks

---
+++ liquid_monitor.lua
@@ -0,0 +1,34 @@
+-- Liquid Monitor
+-- by Bratta
+--
+-- Run on a computer to the left of a 
+-- Nuclear Information Reader (from Misc. Peripherals)
+-- with the proper card paired with your Railcraft tank
+-- It will then toggle redstone in the front of the computer
+-- based on the tank being full or reaching a certain liquid
+-- threshold
+
+local card_slot = 1
+local reader_side = "right"
+local redstone_side = "front"
+local threshold_pct = 0.75  -- Percent to reach before turning on redstone signal
+local threshold = capacity * threshold_pct
+
+reader = peripheral.wrap(reader_side)
+while true do
+  term.clear()
+  term.setCursorPos(1,1)
+  a, b, c, info = reader.get(card_slot)
+  capacity = info["capacity"]
+  amount = info["amount"]
+  print("Amount: " .. tostring(amount))
+  print("Capacity: " .. tostring(capacity))
+  if amount == capacity then
+    redstone.setOutput(redstone_side, false)
+    print("Tank is at capacity; disabling power.")
+  elseif amount < threshold then
+    redstone.setOutput(redstone_side, true)
+    print("Tank is below threshold; enabling power.")
+  end
+  sleep(5)
+end
